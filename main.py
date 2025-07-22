2256dea772c336f1fd850b8b8fb4bb3c163f6dd1
avinashjay1 <Avinash.Veershetty@bristlecone.com>
2025-07-22 18:28:20 +0530

    Update content of main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+print('Welcome to Demo Project 7')
+
 def main():
-    print('App started with ENV: Republican')
+    print('This is the main logic for project 7')
 
 if __name__ == '__main__':
     main()
